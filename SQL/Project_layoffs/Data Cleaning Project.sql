7457ff5dbcf407e50eb1790c479b7e54a34b530a
Leandro S Oliveira <lescherer@gmail.com>
2025-02-17 17:22:37 -0300

    update

--- SQL/Project_layoffs/Data Cleaning Project.sql
+++ SQL/Project_layoffs/Data Cleaning Project.sql
@@ -25,9 +25,14 @@ WITH duplicate_cte AS
 (
 SELECT *,
 ROW_NUMBER() OVER(
-PARTITION BY company, industry, total_laid_off, percentage_laid_off, `date`) AS row_num
+PARTITION BY company, location,industry, total_laid_off, percentage_laid_off, `date`,
+stage, country, funds_raised_millions) AS row_num
 FROM layoffs_staging
 )
 SELECT *
 FROM duplicate_cte
 WHERE row_num >1;
+
+SELECT *
+FROM layoffs_staging
+WHERE company = 'Casper';
